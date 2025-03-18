68c3e435f092eabdd9433caf4b10b1e4aca993cd
Adil Aslam <aaliyanaslam22@gmail.com>
2025-03-18 16:00:18 +0500

    nav modification done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   return (
     <>
    <nav>
-    <div className ="bg-[#feee00] p-1.5 pl-3 flex items-center gap-4 ">
+    <div className ="bg-[#feee00] p-1.5 pl-3 flex items-center justify-evenly gap-4 ">
       {/* noon logo */}
      <span><img src="https://f.nooncdn.com/s/app/com/noon/design-system/logos/noon-logo-en.svg" className='h-12 w-21 cursor-pointer' alt="" /></span>
      {/* location */}
@@ -27,9 +27,9 @@ const Navbar = () => {
 
     {/* Language */}
 
-    <div className='flex '>
+    <div className='flex lg:block md:hidden sm:hidden'>
       <div className='font-normal  text-gray-600 hover:opacity-70 duration-300 opacity-100 cursor-pointer'>
-        Language <span className=' m-2 text-gray-500 font-light border w-10'>
+        Language <span className=' m-2 text-gray-500 font-light border-l w-10 opacity-45'>
           
           </span>
       </div> 
@@ -41,7 +41,7 @@ const Navbar = () => {
 
 <div className='flex items-center '>
       <div className='font-normal  text-gray-600 flex items-center hover:opacity-70 duration-300 opacity-100 cursor-pointer'>
-        Login <span><img src={Loginlogo} className='m-1' alt="" /></span><span className=' m-2 text-gray-500 font-light border h-5'>
+        Login <span><img src={Loginlogo} className='m-1' alt="" /></span><span className=' m-2 text-gray-500 opacity-45 font-light border-l h-5'>
           
           </span>
       </div> 
