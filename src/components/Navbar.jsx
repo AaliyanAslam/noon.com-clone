e6c111ee9fecd065ed0a283ea995aeb6a5b09b7c
Adil Aslam <aaliyanaslam22@gmail.com>
2025-03-18 22:30:02 +0500

    more nav responsiveness done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
-import Loginlogo from '/src/assets/loginlogo.png';
-import Cartlogo from '/src/assets/cartlogo.png';
-import Heartlogo from '/src/assets/heartlogo.png';
+
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Cartlogo from '/src/assets/cartlogo.svg';
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
 
 
 const Navbar = () => {
   return (
     <>
    <nav>
-    <div className ="bg-[#feee00] p-1.5 pl-3 flex items-center  gap-4 sm:gap-3 md:gap-4 lg:gap-6">
+    <div className ="bg-[#feee00] p-1.5 pl-3 flex items-center 
+     gap-4 sm:gap-3 md:gap-4 lg:gap-5 xl:gap-9 sm:bg-[#feee00] md:bg-[#feee00] lg:bg-[#feee00] xl:bg-[#feee00]">
       {/* noon logo */}
-     <span><img src="https://f.nooncdn.com/s/app/com/noon/design-system/logos/noon-logo-en.svg" className='h-12 w-21 cursor-pointer' alt="" /></span>
+     <span><img src="https://f.nooncdn.com/s/app/com/noon/design-system/logos/noon-logo-en.svg" className='h-10 w-18 cursor-pointer sm:h-9 sm:w-18  md:h-10 md:w-19  lg:h-12 lg:w-20 ' alt="" /></span>
      {/* location */}
      <div className='  hidden sm:hidden text-sm font-[300] text-gray-600 gap-2 hover:opacity-70 cursor-pointer duration-300'>
       <div><img src="https://t4.ftcdn.net/jpg/08/85/03/45/360_F_885034550_FL2YBZA2jjLYjLznnHFwqrSPjIhmzDub.jpg" className='h-4 rounded opacity-100 ' alt="" /></div>
@@ -22,12 +26,12 @@ const Navbar = () => {
     {/* Search Bar */}
 
     <div className=''>
-      <input type="text" placeholder='What are you looking for?' className='bg-white rounded h-8.5 text-gray-600 text-sm w-50 sm:w-120 md:w-160 lg:w-185 xl:w-200 focus:outline-none p-3' />
+      <input type="text" placeholder='What are you looking for?' className='bg-white rounded h-8.5 text-xs sm:text-sm ms:text:sm lg:text-sm xl:text-sm text-gray-600  w-50 sm:w-120 md:w-130 lg:w-185 xl:w-200 focus:outline-none p-3' />
     </div> 
 
     {/* Language */}
 
-    <div className='  items-center hidden lg:block md:hidden sm:hidden '>
+    <div className='  items-center hidden lg:hidden md:hidden sm:hidden xl:block '>
       <div className=' flex items-center text-sm  text-gray-600 hover:opacity-70 duration-300 opacity-100 cursor-pointer'>
         Language <span className=' m-2 text-gray-500 font-light border-l w-0 h-4 opacity-45 hidden sm:hidden md:hidden lg:block'>
           
@@ -39,9 +43,9 @@ const Navbar = () => {
 {/* Login */}
 
 
-<div className='hidden sm:hidden md:hidden lg:block '>
-      <div className='font-normal text-sm text-gray-600 flex items-center hover:opacity-70 duration-300 opacity-100 cursor-pointer'>
-        Login <span><img src={Loginlogo}  className='m-1 h-3.5' alt="" /></span><span className=' m-2 text-gray-500 opacity-45 font-light border-l h-5'>
+<div className=' sm:block md:block lg:block '>
+      <div className='font-normal text-xs sm:text-sm ms:text:sm lg:text-sm xl:text-sm text-gray-600 flex items-center hover:opacity-70 duration-300 opacity-100 cursor-pointer'>
+        Login <span className='ml-2'><FontAwesomeIcon icon={faUser}/>  </span><span className=' m-2 text-gray-500 opacity-45 font-light border-l h-5 hidden sm:hidden md:hidden lg:block xl:block'>
           
           </span>
       </div> 
@@ -51,12 +55,12 @@ const Navbar = () => {
 {/* wish logo */}
 
 <div className='hover:opacity-70 duration-300 opacity-100'>
-  <img src={Heartlogo} alt=""  className=' cursor-pointer'/>
+<FontAwesomeIcon className='opacity-60'  icon={faHeart}/> 
 </div>
 
 {/* cartlogo */}
-<div className='hover:opacity-70 duration-300 opacity-100 cursor-pointer hidden sm:hidden md:block  '>
-  <img src={Cartlogo} alt="" />
+<div className='hover:opacity-70 duration-300 opacity-100 cursor-pointer hidden sm:hidden md:block lg:block '>
+ <img className='opacity-60 '  src={Cartlogo} alt="" />  
 </div>
 
     </div>
