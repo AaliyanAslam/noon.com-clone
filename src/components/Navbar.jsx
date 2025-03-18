9cd4270ae702aed3de9394b19eeb6aa8b0d44b89
Adil Aslam <aaliyanaslam22@gmail.com>
2025-03-18 14:39:50 +0500

    navbar  done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import Loginlogo from '/src/assets/loginlogo.png';
+import Cartlogo from '/src/assets/cartlogo.png';
+import Heartlogo from '/src/assets/heartlogo.png';
+
 
 const Navbar = () => {
   return (
@@ -6,9 +10,9 @@ const Navbar = () => {
    <nav>
     <div className ="bg-[#feee00] p-1.5 pl-3 flex items-center gap-4 ">
       {/* noon logo */}
-     <span><img src="https://f.nooncdn.com/s/app/com/noon/design-system/logos/noon-logo-en.svg" className='h-12 w-21' alt="" /></span>
+     <span><img src="https://f.nooncdn.com/s/app/com/noon/design-system/logos/noon-logo-en.svg" className='h-12 w-21 cursor-pointer' alt="" /></span>
      {/* location */}
-     <div className='flex  items-center text-sm font-[300] text-gray-600 gap-2 hover:opacity-70 cursor-pointer duration-300'>
+     <div className='flex items-center text-sm font-[300] text-gray-600 gap-2 hover:opacity-70 cursor-pointer duration-300'>
       <div><img src="https://t4.ftcdn.net/jpg/08/85/03/45/360_F_885034550_FL2YBZA2jjLYjLznnHFwqrSPjIhmzDub.jpg" className='h-4 rounded opacity-100 ' alt="" /></div>
       <div className='flex flex-col  items-start'>
       <div className='font-normal'>Delever to</div>
@@ -17,9 +21,44 @@ const Navbar = () => {
     </div>
     {/* Search Bar */}
 
-    <div>
-      <input type="text" className='bg-white rounded h-8.5 text-gray-600 text-sm lg:w-190 md:w-120 sm:w-100 s:w-80 focus:outline-none p-3' />
+    <div className=''>
+      <input type="text" placeholder='What are you looking for?' className='bg-white rounded h-8.5 text-gray-600 text-sm lg:w-190 md:w-120 sm:w-100 s:w-80 focus:outline-none p-3' />
     </div> 
+
+    {/* Language */}
+
+    <div className='flex '>
+      <div className='font-normal  text-gray-600 hover:opacity-70 duration-300 opacity-100 cursor-pointer'>
+        Language <span className=' m-2 text-gray-500 font-light border w-10'>
+          
+          </span>
+      </div> 
+    </div>
+
+
+{/* Login */}
+
+
+<div className='flex items-center '>
+      <div className='font-normal  text-gray-600 flex items-center hover:opacity-70 duration-300 opacity-100 cursor-pointer'>
+        Login <span><img src={Loginlogo} className='m-1' alt="" /></span><span className=' m-2 text-gray-500 font-light border h-5'>
+          
+          </span>
+      </div> 
+    </div>
+
+
+{/* wish logo */}
+
+<div className='hover:opacity-70 duration-300 opacity-100'>
+  <img src={Heartlogo} alt=""  className=' cursor-pointer'/>
+</div>
+
+{/* cartlogo */}
+<div className='hover:opacity-70 duration-300 opacity-100 cursor-pointer'>
+  <img src={Cartlogo} alt="" />
+</div>
+
     </div>
 
     
