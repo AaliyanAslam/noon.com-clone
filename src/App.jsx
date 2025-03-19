ad3b32e70f4539f85739c691d84e16925150b7c6
Adil Aslam <aaliyanaslam22@gmail.com>
2025-03-19 16:20:03 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import NavProducts from './components/NavProducts'
 import BottomNav from './components/BottomNav'
-import DeliveryLocationNav from './components/DeliveryLocationNav'
+import DeliveryLocationNav from '/src/components/DeliveryLocationNav'
 
 const App = () => {
+ 
+  
   return (
     <>
     <Navbar/>
