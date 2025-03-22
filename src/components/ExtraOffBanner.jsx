8c343af1a3ac74021625ccf199117c6e3233e2cf
Adil Aslam <aaliyanaslam22@gmail.com>
2025-03-22 23:54:15 +0500

    Update ExtraOffBanner.jsx

--- src/components/ExtraOffBanner.jsx
+++ src/components/ExtraOffBanner.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const ExtraOffBanner = () => {
   return (
     <>
-    <nav className='mt-1 cursor-pointer'>
-      <div>
-        <img src="https://f.nooncdn.com/mpcms/EN0001/assets/a6706a76-d65d-4781-aa66-fbcaa92affcc.png" alt="" />
-      </div>
-    </nav>
-    
-    </>
-  )
-}
+<div className="container">
 
-export default ExtraOffBanner
+
+      <div className="m-2 ">
+        <img
+          className="rounded cursor-pointer"
+          src="https://f.nooncdn.com/mpcms/EN0001/assets/a6706a76-d65d-4781-aa66-fbcaa92affcc.png"
+          alt=""
+        />
+      </div>
+</div>
+
+    </>
+  );
+};
+
+export default ExtraOffBanner;
