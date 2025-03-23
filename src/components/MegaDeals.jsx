d870be7259f5a93880226cc99a85d3458a8f6892
Adil Aslam <aaliyanaslam22@gmail.com>
2025-03-23 11:15:12 +0500

    reason too buy and mega deal almos don

--- src/components/MegaDeals.jsx
+++ src/components/MegaDeals.jsx
@@ -4,43 +4,41 @@ const MegaDeals = () => {
   return (
     <>
       <div className="grid grid-cols-12 gap-1 m-1">
+        {/* More Reasons to shop */}
         <div className="  col-span-4 bg-amber-200 p-1  ">
           <img
             src="https://f.nooncdn.com/mpcms/EN0002/assets/184ca7f5-53e5-4b7c-86a9-c5e861b7fbab.png"
             alt=""
           />
           <div className="product-container grid grid-cols-2 ">
-              <div className="max-w-sm mx-auto bg-white rounded-lg shadow-lg overflow-hidden cursor-pointer">
-                <img
-                  src="https://f.nooncdn.com/mpcms/EN0001/assets/58148da9-50f1-49b4-939a-c9e9ffe4b756.png"
-                  alt="Card Image"
-                  className=" h-48 object-cover"
-                />
-              </div>
-              <div className="max-w-sm mx-auto bg-white rounded-lg shadow-lg overflow-hidden cursor-pointer">
-                <img
-                  src="https://f.nooncdn.com/mpcms/EN0001/assets/58148da9-50f1-49b4-939a-c9e9ffe4b756.png"
-                  alt="Card Image"
-                  className=" h-48 object-cover"
-                />
-              </div>
-              <div className="max-w-sm mx-auto bg-white rounded-lg shadow-lg overflow-hidden cursor-pointer">
-                <img
-                  src="https://f.nooncdn.com/mpcms/EN0001/assets/58148da9-50f1-49b4-939a-c9e9ffe4b756.png"
-                  alt="Card Image"
-                  className=" h-48 object-cover"
-                />
-              </div>
-              <div className="max-w-sm mx-auto bg-white rounded-lg shadow-lg overflow-hidden cursor-pointer">
-                <img
-                  src="https://f.nooncdn.com/mpcms/EN0001/assets/58148da9-50f1-49b4-939a-c9e9ffe4b756.png"
-                  alt="Card Image"
-                  className=" h-48 object-cover"
-                />
-              </div>
-
-
-           
+            <div className="max-w-sm mx-auto bg-white rounded-lg shadow-lg overflow-hidden cursor-pointer">
+              <img
+                src="https://f.nooncdn.com/mpcms/EN0001/assets/58148da9-50f1-49b4-939a-c9e9ffe4b756.png"
+                alt="Card Image"
+                className=" h-23 sm:h-24 md:h-28 lg:h-48 xl:h-48 object-cover"
+              />
+            </div>
+            <div className="max-w-sm mx-auto bg-white rounded-lg shadow-lg overflow-hidden cursor-pointer">
+              <img
+                src="https://f.nooncdn.com/mpcms/EN0001/assets/58148da9-50f1-49b4-939a-c9e9ffe4b756.png"
+                alt="Card Image"
+                className=" h-23 sm:h-24 md:h-28 lg:h-48 xl:h-48 object-cover"
+              />
+            </div>
+            <div className="max-w-sm mx-auto bg-white rounded-lg shadow-lg overflow-hidden cursor-pointer">
+              <img
+                src="https://f.nooncdn.com/mpcms/EN0001/assets/58148da9-50f1-49b4-939a-c9e9ffe4b756.png"
+                alt="Card Image"
+                className=" h-23 sm:h-24 md:h-28 lg:h-48 xl:h-48 object-cover"
+              />
+            </div>
+            <div className="max-w-sm mx-auto bg-white rounded-lg shadow-lg overflow-hidden cursor-pointer">
+              <img
+                src="https://f.nooncdn.com/mpcms/EN0001/assets/58148da9-50f1-49b4-939a-c9e9ffe4b756.png"
+                alt="Card Image"
+                className=" h-23 sm:h-24 md:h-28 lg:h-48 xl:h-48 object-cover"
+              />
+            </div>
           </div>
         </div>
         <div className=" border col-span-4">
@@ -48,13 +46,98 @@ const MegaDeals = () => {
             src="https://f.nooncdn.com/mpcms/EN0001/assets/c28b8a63-e38b-4d80-916f-1a0bb79d57a3.png"
             alt=""
           />
+
+{/* Mega deals */}
+
+<div className="col-span-4 bg-amber-200 p-1  ">
+          <img
+            src="https://f.nooncdn.com/p/pzsku/ZB5B4E87138503D23F313Z/45/_/1725131789/c879a173-43ca-4dd6-ad92-b004a6cd8232.jpg?width=240"
+            alt=""
+          />
+          <div className="product-container grid grid-cols-2 ">
+            <div className="max-w-sm mx-auto bg-white rounded-lg shadow-lg overflow-hidden cursor-pointer">
+              <img
+                src="https://f.nooncdn.com/p/v1669040907/N38849954A_2.jpg?width=240"
+                alt="Card Image"
+                className=" h-23 sm:h-24 md:h-28 lg:h-29 xl:h-48 object-cover"
+              />
+            </div>
+            <div className="max-w-sm mx-auto bg-white rounded-lg shadow-lg overflow-hidden cursor-pointer">
+              <img
+                src="https://f.nooncdn.com/p/pnsku/N70063303V/45/_/1739967032/46a55577-5243-4b22-b6a8-b70f512f64d6.jpg?width=240"
+                alt="Card Image"
+                className=" h-23 sm:h-24 md:h-28 lg:h-48 xl:h-48 object-cover"
+              />
+            </div>
+            <div className="max-w-sm mx-auto bg-white rounded-lg shadow-lg overflow-hidden cursor-pointer">
+              <img
+                src="https://f.nooncdn.com/p/pzsku/Z1721D54042AD6DC47338Z/45/_/1698990673/ab1bc35d-afcf-42d3-a753-0e6e9abe04c5.jpg?width=240"
+                alt="Card Image"
+                className=" h-23 sm:h-24 md:h-28 lg:h-48 xl:h-48 object-cover"
+              />
+            </div>
+            {/* <div className="max-w-sm mx-auto bg-white rounded-lg shadow-lg overflow-hidden cursor-pointer">
+              <img
+                src="https://f.nooncdn.com/mpcms/EN0001/assets/58148da9-50f1-49b4-939a-c9e9ffe4b756.png"
+                alt="Card Image"
+                className=" h-23 sm:h-24 md:h-28 lg:h-48 xl:h-48 object-cover"
+              />
+            </div> */}
+          </div>
+        </div>
+
+
         </div>
         <div className=" border col-span-4">
           <img
             src="https://f.nooncdn.com/mpcms/EN0002/assets/733bdc4a-5b38-459a-8b8d-79093afa0423.png"
             alt=""
           />
+
+          {/* infocus */}
+          <div className="  col-span-4 bg-amber-200 p-1  ">
+          <img
+            src="https://f.nooncdn.com/mpcms/EN0002/assets/184ca7f5-53e5-4b7c-86a9-c5e861b7fbab.png"
+            alt=""
+          />
+          <div className="product-container grid grid-cols-2 ">
+            <div className="max-w-sm mx-auto bg-white rounded-lg shadow-lg overflow-hidden cursor-pointer">
+              <img
+                src="https://f.nooncdn.com/mpcms/EN0001/assets/58148da9-50f1-49b4-939a-c9e9ffe4b756.png"
+                alt="Card Image"
+                className=" h-23 sm:h-24 md:h-28 lg:h-48 xl:h-48 object-cover"
+              />
+            </div>
+            <div className="max-w-sm mx-auto bg-white rounded-lg shadow-lg overflow-hidden cursor-pointer">
+              <img
+                src="https://f.nooncdn.com/mpcms/EN0001/assets/58148da9-50f1-49b4-939a-c9e9ffe4b756.png"
+                alt="Card Image"
+                className=" h-23 sm:h-24 md:h-28 lg:h-48 xl:h-48 object-cover"
+              />
+            </div>
+            <div className="max-w-sm mx-auto bg-white rounded-lg shadow-lg overflow-hidden cursor-pointer">
+              <img
+                src="https://f.nooncdn.com/mpcms/EN0001/assets/58148da9-50f1-49b4-939a-c9e9ffe4b756.png"
+                alt="Card Image"
+                className=" h-23 sm:h-24 md:h-28 lg:h-48 xl:h-48 object-cover"
+              />
+            </div>
+            <div className="max-w-sm mx-auto bg-white rounded-lg shadow-lg overflow-hidden cursor-pointer">
+              <img
+                src="https://f.nooncdn.com/mpcms/EN0001/assets/58148da9-50f1-49b4-939a-c9e9ffe4b756.png"
+                alt="Card Image"
+                className=" h-23 sm:h-24 md:h-28 lg:h-48 xl:h-48 object-cover"
+              />
+            </div>
+          </div>
         </div>
+        </div>
+
+{/* MegaDeals */}
+
+
+
+
       </div>
     </>
   );
